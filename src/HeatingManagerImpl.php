edc1e33508265d899516b8225169f657b042a7ca
Michael Große <michael.grosse@wikimedia.de>
2023-03-13 13:36:01 +0100

    Add access modifier to public method

--- src/HeatingManagerImpl.php
+++ src/HeatingManagerImpl.php
@@ -11,7 +11,7 @@ class HeatingManagerImpl {
 	public function setHomeSocketManager( HomeSocketManager $homeSocketManager ): void {
 		$this->homeSocketManager = $homeSocketManager;
 	}
-	function manageHeating( float $t, float $threshold ): void {
+	public function manageHeating( float $t, float $threshold ): void {
 		if ( $t < $threshold ) {
 			$this->homeSocketManager->send( 'heater.home', 9999, 'on' );
 		} elseif ( $t > $threshold ) {
